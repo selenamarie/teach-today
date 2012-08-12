1bc1223f10b19cc6e063ecd6bb338072d0860ab9
Selena Deckelmann <selenamarie@gmail.com>
2012-08-11 18:56:26 -0700

    moved urls around for promises

---
+++ lessons/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+urlpatterns = patterns('lessons.views',
+	# Lessons (must be plural in url)
+    url(r'^$', 'index'),
+    url(r'^(?P<lesson_id>\d+)/$', 'detail'),
+    url(r'^promises/$', 'promises'),
+    url(r'^promises/(?P<promise_id>\d+)/$', 'promise_detail'),
+    url(r'^promises/(?P<promise_id>\d+)/keep$', 'keep'),
+
+    # Uncomment the next line to enable the admin:
+)
--- teach/urls.py
+++ teach/urls.py
@@ -5,23 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'teach.views.home', name='home'),
-    # url(r'^teach/', include('teach.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-	# Lessons (must be plural in url)
-    url(r'^lessons/$', 'lessons.views.index'),
-    url(r'^lessons/(?P<lesson_id>\d+)/$', 'lessons.views.detail'),
-
-	# Promises
-    url(r'^promises/$', 'lessons.views.promises'),
-    url(r'^promises/(?P<promise_id>\d+)/$', 'lessons.views.promise_detail'),
-    url(r'^promises/(?P<promise_id>\d+)/keep$', 'lessons.views.keep'),
-    #url(r'^promise/(?P<promise_id>\d+)/status$', 'promise.views.status'),
-
-    # Uncomment the next line to enable the admin:
+	url(r'^lessons/', include('lessons.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
