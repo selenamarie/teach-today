c01f3e28476e20ffe6c01a158de07c7d8969db03
Selena Deckelmann <selenamarie@gmail.com>
2012-08-20 15:05:07 -0500

    Whitespace cleanup, add username to Profile admin

--- lessons/models.py
+++ lessons/models.py
@@ -1,26 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Lesson(models.Model):
-	name = models.CharField(max_length=200)
-	url = models.URLField()
-	pub_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+    pub_date = models.DateTimeField(auto_now_add=True)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 class Promise(models.Model):
-	who = models.CharField(max_length=200)
-	when = models.DateTimeField('when')
-	lesson = models.ForeignKey(Lesson)
-	done = models.BooleanField(default=False)
+    who = models.CharField(max_length=200)
+    when = models.DateTimeField('when')
+    lesson = models.ForeignKey(Lesson)
+    done = models.BooleanField(default=False)
+    made_by = models.ForeignKey(User)
 
-	def __unicode__(self):
-		return "Teach <a href='/lessons/%i/'>%s</a> for <strong>%s</strong> on " % (self.lesson.id, self.lesson.name, self.who)
+    def __unicode__(self):
+        return "Teach <a href='/lessons/%i/'>%s</a> for <strong>%s</strong> on " % (self.lesson.id, self.lesson.name, self.who)
 
 class Assessment(models.Model):
-	question = models.CharField(max_length=200)
-	pre = models.TextField()
-	post = models.TextField()
-	who = models.CharField(max_length=200)
+    question = models.CharField(max_length=200)
+    pre = models.TextField()
+    post = models.TextField()
+    who = models.CharField(max_length=200)
--- teach/models.py
+++ teach/models.py
@@ -5,9 +5,11 @@ from django.db.models.signals import post_save
 class UserProfile(models.Model):
     # This field is required.
     user = models.OneToOneField(User)
-
     # Other fields here
     favorite_animal = models.CharField(max_length=20, default="Cats")
+
+    def __unicode__(self):
+        return self.user.username
 
 # definition of UserProfile from above
 # ...
