667162520b62e380e0ece21a67d113b0948d7bed
Selena Deckelmann <selenamarie@gmail.com>
2012-08-23 01:00:34 +0200

    More improvement tweaking per convo with @oliver_ponder

--- lessons/views.py
+++ lessons/views.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.contrib.auth.models import User
 
+import datetime
+
 from django.contrib.auth.decorators import login_required
 
 from lessons.models import Lesson, Promise, Assessment, AssessmentResponse
