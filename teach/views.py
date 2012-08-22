762fcc1b99af388d7738334629af728fea295303
Selena Deckelmann <selenamarie@gmail.com>
2012-08-22 12:53:28 +0200

    for want of a comma

--- teach/views.py
+++ teach/views.py
@@ -61,5 +61,5 @@ def profile_create(request, user_id):
     else:
         up = UserProfile()
         form = UserProfileForm()
-        return render_to_response('profiles/create_profile.html', {'profile': up, 'form': form}
+        return render_to_response('profiles/create_profile.html', {'profile': up, 'form': form},
                                context_instance=RequestContext(request))
